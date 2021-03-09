d559b120b19ffd77740363a915ae1a242fa62f67
Gerard Keane <gerard.keane@dfkeane.com>
2021-03-09 13:17:32 +0000

    added ranking support

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -5,6 +5,7 @@
       <li>The box appeared after: <strong>{{ ((delay % 60000) / 1000).toFixed(2) }} seconds</strong></li>
       <li>You reacted in <strong>{{score}}ms</strong>. That is <strong>{{ ((score % 60000) / 1000).toFixed(2) }} seconds</strong>.</li>
     </ul>
+    <p>{{ ranking }}</p>
   </div>
 </template>
 
@@ -12,9 +13,18 @@
 export default {
   name: 'Results',
   props: ['delay', 'score'],
-  computed : {
-
-  }
+  data() {
+    return {
+      ranking: null
+    }
+  },
+  mounted() {
+    if (this.score < 250) {
+      this.ranking = "Superb reactions. You are a zen master."
+      } else if (this.score > 250) {
+        this.ranking = "Were you asleep?"
+        }
+  },
 };
 </script>
 
