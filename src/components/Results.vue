7e0834ac261f60a91bdf70d7155d9a9803f2620a
Grrrotbag <gerard.keane@dfkeane.com>
2021-03-09 13:58:15 +0000

    new ranking and formatting

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -2,8 +2,13 @@
   <div>
     <h2>These are the results:</h2>
     <ul>
-      <li>The box appeared after: <strong>{{ ((delay % 60000) / 1000).toFixed(2) }} seconds</strong></li>
-      <li>You reacted in <strong>{{score}}ms</strong>. That is <strong>{{ ((score % 60000) / 1000).toFixed(2) }} seconds</strong>.</li>
+      <li>
+        The box appeared after: <strong>{{ ((delay % 60000) / 1000).toFixed(2) }} seconds</strong>
+      </li>
+      <li>
+        You reacted in <strong>{{ score }}ms</strong>. That is
+        <strong>{{ ((score % 60000) / 1000).toFixed(2) }} seconds</strong>.
+      </li>
     </ul>
     <p>{{ ranking }}</p>
   </div>
@@ -11,23 +16,25 @@
 
 <script>
 export default {
-  name: 'Results',
-  props: ['delay', 'score'],
+  name: "Results",
+  props: ["delay", "score"],
   data() {
     return {
-      ranking: null
-    }
+      ranking: null,
+    };
   },
   mounted() {
-    if (this.score < 250) {
-      this.ranking = "Superb reactions. You are a zen master."
-      } else if (this.score < 350) {
-        this.ranking = "Not too shabby."
-        } else if (this.score < 450) {
-          this.ranking = "Getting a little slow in your old age?"
-          } else {
-            this.ranking = "What, were you asleep or something?"
-          }
+    if (this.score < 150) {
+      this.ranking = "Woah. You must have cheated you're so fast!";
+    } else if (this.score < 250) {
+      this.ranking = "Superb reactions. You are a zen master.";
+    } else if (this.score < 350) {
+      this.ranking = "Not too shabby.";
+    } else if (this.score < 450) {
+      this.ranking = "Getting a little slow in your old age?";
+    } else {
+      this.ranking = "What, were you asleep or something?";
+    }
   },
 };
 </script>
@@ -35,5 +42,5 @@ export default {
 <style>
 ul {
   list-style-type: none;
-  }
+}
 </style>
